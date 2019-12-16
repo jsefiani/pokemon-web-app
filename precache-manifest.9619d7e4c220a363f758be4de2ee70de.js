f9ca00a8eaca8a07dd5ceceaa8bf52364fddf4cb
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-17 00:05:42 +0100

    Updates

---
+++ precache-manifest.9619d7e4c220a363f758be4de2ee70de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a8fdbd9e048bf07d17e74b677bd246",
+    "url": "/pokemon-web-app/index.html"
+  },
+  {
+    "revision": "2af5eec7d17d92639a9f",
+    "url": "/pokemon-web-app/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "2af5eec7d17d92639a9f",
+    "url": "/pokemon-web-app/static/js/2.f762f4a5.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/pokemon-web-app/static/js/2.f762f4a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c0d2bf2ac3e1fa6f10",
+    "url": "/pokemon-web-app/static/js/main.533f1adc.chunk.js"
+  },
+  {
+    "revision": "b33647178d25b9dfafbc",
+    "url": "/pokemon-web-app/static/js/runtime-main.708cdde8.js"
+  }
+]);
