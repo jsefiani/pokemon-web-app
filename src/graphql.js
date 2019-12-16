b59b80482220c09bc4a80662d05b6befc6976c87
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 01:00:58 +0100

    feature: initialize cache

--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,18 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
+import { resolvers } from './resolvers';
+import { typeDefs } from './schema';
+
+// ? Configure Apollo
+const cache = new InMemoryCache();
 export default new ApolloClient({
-  uri: process.env.REACT_APP_POKE_ENDPOINT
+	uri: process.env.REACT_APP_POKE_ENDPOINT,
+	clientState: {
+		cache,
+		defaults: {
+			selectedPokemonName: ''
+		},
+		typeDefs,
+		resolvers
+	}
 });
