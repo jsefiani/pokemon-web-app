9adabb69bde6203919c2112db4186a8c5adad119
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 20:09:12 +0100

    feature(WIP): add pokemon move

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,9 @@
 import { gql } from 'apollo-boost';
 
 export const resolvers = {
+	Pokemon: {
+		selectedMoves: () => []
+	},
 	Mutation: {
 		setPokemonName: (_root, { name }, { cache }) => {
 			const GET_POKEMON_NAME = gql`
@@ -14,6 +17,50 @@ export const resolvers = {
 			});
 
 			return name;
+		},
+		addPokemonMove: (
+			_root,
+			{ id, name, selectedPokemonMove },
+			{ cache }
+		) => {
+			console.log('addPokemonMove', { id });
+			const GET_POKEMON = gql`
+				query GetPokemon($name: String!) {
+					Pokemon(name: $name) @client {
+						selectedMoves
+					}
+				}
+			`;
+			const { Pokemon } = cache.readQuery({
+				id,
+				query: GET_POKEMON,
+				variables: { name }
+			});
+
+			if (Pokemon.selectedMoves.length > 3) return Pokemon.selectedMoves;
+
+			const data = {
+				Pokemon: {
+					...Pokemon,
+					selectedMoves: [
+						...Pokemon.selectedMoves,
+						{ ...selectedPokemonMove, __typename: 'PokemonMove' }
+					]
+				}
+			};
+
+			cache.writeData({
+				id,
+				query: GET_POKEMON,
+				data,
+				variables: { name }
+			});
+
+			console.log('addMove', {
+				selectedPokemonMove,
+				pokemon: Pokemon
+			});
+			return Pokemon.selectedMoves;
 		}
 	}
 };
