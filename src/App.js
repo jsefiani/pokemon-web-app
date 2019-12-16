59c27aab5995eb1d5b37cbd0aeb522935da81750
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 01:00:32 +0100

    feature: adding PokemonList and PokemonDetails comp to app view

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, { Component } from "react";
-import { ApolloProvider } from "react-apollo";
-import styled from "styled-components";
-import client from "./graphql";
+import React, { Component } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import styled from 'styled-components';
+import client from './graphql';
+
+import PokemonList from './components/PokemonList';
+import PokemonDetails from './components/PokemonDetails';
 
 const Logo = styled.img`
-  display: block;
-  margin: 0 auto;
-  max-width: 200px;
+	display: block;
+	margin: 0 auto;
+	max-width: 200px;
 `;
 
 const Container = styled.div`
-  max-width: 980px;
-  margin: 0 auto;
+	max-width: 980px;
+	margin: 0 auto;
+`;
+
+const Layout = styled.div`
+	display: flex;
 `;
 
 class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <Container>
-          <Logo src="https://vignette.wikia.nocookie.net/logopedia/images/2/2b/Pokemon_2D_logo.svg/revision/latest/scale-to-width-down/639?cb=20170115063554" />
-
-          <div>BUILD YOUR APP here</div>
-        </Container>
-      </ApolloProvider>
-    );
-  }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<Container>
+					<Logo src="https://vignette.wikia.nocookie.net/logopedia/images/2/2b/Pokemon_2D_logo.svg/revision/latest/scale-to-width-down/639?cb=20170115063554" />
+					<Layout>
+						<PokemonList />
+						<PokemonDetails />
+					</Layout>
+				</Container>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
