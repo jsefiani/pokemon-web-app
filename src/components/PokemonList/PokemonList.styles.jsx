296b490952622b567c90bcd091624928c199dcd2
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 01:04:03 +0100

    feature: init

---
+++ src/components/PokemonList/PokemonList.styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PokemonListContainer = styled.ul`
+	padding: 0;
+	height: 150px;
+	overflow-y: scroll;
+	width: 200px;
+`;
+
+export const PokemonListItemContainer = styled.li`
+	cursor: pointer;
+	padding: 10px 5px;
+`;
+
+export const Container = styled.div`
+	margin-right: 30px;
+`;
