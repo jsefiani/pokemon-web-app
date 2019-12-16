0df8eb34fa40db93cb3d3f1cfcf4f7473cc978d7
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 22:50:44 +0100

    refactor: loop through selected moves

--- src/components/PokemonSelectedMoves/index.jsx
+++ src/components/PokemonSelectedMoves/index.jsx
@@ -2,32 +2,15 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const PokemonSelectedMoves = ({ selectedMoves }) => {
-	console.log({ selectedMoves });
-	return (
-		<>
-			<Row>
-				<Col>
-					<span>{selectedMoves[0]?.learnMethod}</span>
-					<span>{selectedMoves[0]?.name}</span>
-				</Col>
-				<Col>
-					<span>{selectedMoves[1]?.learnMethod}</span>
-					<span>{selectedMoves[1]?.name}</span>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<span>{selectedMoves[2]?.learnMethod}</span>
-					<span>{selectedMoves[2]?.name}</span>
-				</Col>
-				<Col>
-					<span>{selectedMoves[3]?.learnMethod}</span>
-					<span>{selectedMoves[3]?.name}</span>
-				</Col>
-			</Row>
-		</>
-	);
-};
+const PokemonSelectedMoves = ({ selectedMoves }) => (
+	<Row>
+		{selectedMoves.map(({ learnMethod, name }) => (
+			<Col xs={6} key={`${learnMethod}_${name}`}>
+				{learnMethod}
+				<Col>{name}</Col>
+			</Col>
+		))}
+	</Row>
+);
 
 export default PokemonSelectedMoves;
