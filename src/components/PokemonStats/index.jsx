9f29d7ab10e04da17088049dbaac04c99eb5ca9f
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-16 22:51:01 +0100

    refactor: loop through pokemon stats

--- src/components/PokemonStats/index.jsx
+++ src/components/PokemonStats/index.jsx
@@ -3,38 +3,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const PokemonStats = ({ stats }) => (
-	<>
-		<Row>
-			<Col>
-				<span>{stats[0].name}</span>
-				<span>{stats[0].value}</span>
+	<Row>
+		{stats.map(({ name, value }) => (
+			<Col xs={6} key={`${name}_${value}`}>
+				<Row>
+					<Col>{name}</Col>
+					<Col>{value}</Col>
+				</Row>
 			</Col>
-			<Col>
-				<span>{stats[1].name}</span>
-				<span>{stats[1].value}</span>
-			</Col>
-		</Row>
-		<Row>
-			<Col>
-				<span>{stats[2].name}</span>
-				<span>{stats[2].value}</span>
-			</Col>
-			<Col>
-				<span>{stats[3].name}</span>
-				<span>{stats[3].value}</span>
-			</Col>
-		</Row>
-		<Row>
-			<Col>
-				<span>{stats[4].name}</span>
-				<span>{stats[4].value}</span>
-			</Col>
-			<Col>
-				<span>{stats[5].name}</span>
-				<span>{stats[5].value}</span>
-			</Col>
-		</Row>
-	</>
+		))}
+	</Row>
 );
 
 export default PokemonStats;
